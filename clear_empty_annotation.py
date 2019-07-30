f4ba1bc5a18025a1b4ef3b4126547c80ce215656
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2019-07-30 15:41:52 +0200

    Update and rename txtno0.py to clear_empty_annotation.py

--- clear_empty_annotation.py
+++ clear_empty_annotation.py
@@ -11,9 +11,9 @@ delete the empty annotation only in txt file
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-df = pd.read_table('/home/dingjin/no0/tra_val_416.txt', header=None, delimiter='\n')
+df = pd.read_table('annotation.txt', header=None, delimiter='\n')
 df1 = df[0].str.split(' ', expand=True)
 df2 = df1.dropna(axis=0, how='any', thresh=3)
 #print(df2)
-df2.to_csv('/home/dingjin/no0/result.txt', index=False, header=None, sep=' ', line_terminator='\n')
+df2.to_csv('no0annotation.txt', index=False, header=None, sep=' ', line_terminator='\n')
 
