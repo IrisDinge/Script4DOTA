c107e485a205a848b43c10184f7972151315f188
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2019-07-30 15:53:50 +0200

    addLabelTxtall

---
+++ add_all.py
@@ -0,0 +1,29 @@
+import os
+import os.path
+'''
+this code is going to the contect of all .txt in a .txt
+output will be found in the same directory just lick filepath
+
+'''
+
+#print('2')
+def mergeTxt(filepath, outfile):
+
+    k = open(filepath+outfile, 'a+')
+    #print(filepath + outfile)
+
+    for parent, dirnames, filenames in os.walk(filepath):
+        for filepath in filenames:
+            txtPath = os.path.join(parent,filepath)
+            f = open(txtPath)
+            k.write(f.read())
+
+
+
+        k.close()
+
+
+        print('finished')
+
+if __name__ == "__main__":
+    mergeTxt('/home/dingjin/DOTA/tra_416_cp/label/','ky.txt')
