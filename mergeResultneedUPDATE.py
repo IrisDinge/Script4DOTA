becb1db01a1c3420fbd37887e00f32a265d60dc1
IrisDinge <34557032+IrisDinge@users.noreply.github.com>
2019-09-27 14:26:29 +0200

    Update mergeResultneedUPDATE.py

--- mergeResultneedUPDATE.py
+++ mergeResultneedUPDATE.py
@@ -1,4 +1,51 @@
 import pandas as pd
+import os
+
+
+labels = ["small-vehicle",
+          "large-vehicle",
+          "plane",
+          "harbor",
+          "ship",
+          "helicopter",
+          "bridge",
+          "swimming-pool",
+          "baseball-diamond",
+          "roundabout",
+          "tennis-court",
+          "storage-tank",
+          "soccer-ball-field",
+          "basketball-court",
+          "ground-track-field",
+          "container-crane"]
+
+
+for i in labels:
+    data = pd.read_table("/home/dingjin/Yolov3_DOTA/yolo/results/Yolov3_JPEG800/" + "comp4_det_test_" + i + ".txt",
+                         sep=' ', names=['image', 'score', 'x', 'y', 'w', 'h'])
+    data.insert(loc=1, column='id', value=i)
+    data.to_csv("/home/dingjin/Yolov3_DOTA/yolo/results/Yolov3_JPEG800/merge/" + i + ".txt", sep=' ',
+                header=None, index=None)
+    #print(i)
+
+'''
+
+labels = ["small-vehicle",
+          "large-vehicle",
+          "plane",
+          "harbor",
+          "ship",
+          "helicopter",
+          "bridge",
+          "swimming-pool",
+          "baseball-diamond",
+          #"roundabout",
+          #"tennis-court",
+          #"storage-tank",
+          #"soccer-ball-field",
+          #"basketball-court",
+          #"ground-track-field",
+          #"container-crane"]
 
 data = pd.read_csv("/home/dingjin/Yolov3_DOTA/yolo/results/comp4_det_test_baseball-diamond.txt",
                    sep=' ', names=['image', 'score', 'x', 'y', 'w', 'h'])
@@ -7,3 +54,4 @@ data = pd.read_csv("/home/dingjin/Yolov3_DOTA/yolo/results/comp4_det_test_baseba
 data.insert(loc=1, column='id', value='baseball-diamond')
 data.to_csv("/home/dingjin/Yolov3_DOTA/yolo/results/merge/baseball-diamond.txt", sep=' ', header=None, index=None)
 print(data)
+'''
